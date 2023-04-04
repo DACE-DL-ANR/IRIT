7c7245261650805f170b20804934e50e64c18371
anonymousReasoner <83429506+anonymousReasoner@users.noreply.github.com>
2023-04-04 15:36:06 +0200

    adding CallLogMap class

---
+++ src/main/java/org/example/CallLogMap.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.io.File;
+
+public class CallLogMap {
+    String LogmapPath;
+
+    public void setLogmapPath(String logmapPath) {
+        LogmapPath = logmapPath;
+    }
+
+    public void execute(File fileSource, File fileTarget, Double valueOfConf) {
+
+//java -jar target/logmap-matcher-4.0.jar MATCHER source_temp.ttl  target_temp.ttl  ./output f
+
+        StringBuilder arguments = new StringBuilder();
+        //.append(javaPath).append(" -jar ")..append("/IdeaProjects/").append("/IdeaProjects/").
+        arguments.append("java -jar ").append(LogmapPath).append(" MATCHER");
+        arguments.append(fileSource).append(" ");
+        arguments.append(fileTarget).append(" ./output f");
+
+        try {
+
+            Run.runProcess(arguments.toString());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
